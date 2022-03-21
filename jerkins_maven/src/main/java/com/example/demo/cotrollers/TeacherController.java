76bb2d4996ed09aa2d2d41f8379e895f46ad922e
Vinodhini.D <Vinodhini.D@EXIND3010-108.allfon.in>
2022-03-21 12:53:05 +0530

    REQ-102 Get By the Teacher Department

--- jerkins_maven/src/main/java/com/example/demo/cotrollers/TeacherController.java
+++ jerkins_maven/src/main/java/com/example/demo/cotrollers/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,15 @@ public class TeacherController {
 	@GetMapping(path="/teachers")
 	public String getTeachers() {
 		List<String> list = Arrays.asList("Amar","Akbar","Anthony");
+		return list.toString();
+	}
+	
+	@GetMapping(path="/teachers/{dept}")
+	public String getTeachersByDept(@PathVariable("dept") String dept) {
+		List<String> list = Arrays.asList("Amar","Akbar","Anthony");
+		if(dept.equals("ece")) {
+			list = Arrays.asList("Shankar","Salim","Simon");
+		}
 		return list.toString();
 	}
 
