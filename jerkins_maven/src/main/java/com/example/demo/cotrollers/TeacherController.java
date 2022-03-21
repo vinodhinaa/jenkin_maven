ec2548ec82d444d066ee8abfbc049ec1fa0fb146
Vinodhini.D <Vinodhini.D@EXIND3010-108.allfon.in>
2022-03-21 13:06:33 +0530

    Successfully comppleted

--- jerkins_maven/src/main/java/com/example/demo/cotrollers/TeacherController.java
+++ jerkins_maven/src/main/java/com/example/demo/cotrollers/TeacherController.java
@@ -24,7 +24,7 @@ public class TeacherController {
 		if(dept.equals("ece")) {
 			list = Arrays.asList("Shankar","Salim","Simon");
 		}
-		return list.toString();
+		return list.toString();  
 	}
 
 }
